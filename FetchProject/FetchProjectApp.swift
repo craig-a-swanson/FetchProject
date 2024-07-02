3d33a524f84496ea8904b87345e6a5e6a2fd4f1c
craig-a-swanson <cswanson02@me.com>
2024-07-01 23:03:51 -0500

    Initial Commit

---
+++ FetchProject/FetchProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchProjectApp.swift
+//  FetchProject
+//
+//  Created by Craig Swanson on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

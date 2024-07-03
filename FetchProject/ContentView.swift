97bc5e1bbe959da16624fc99eff3a6f8df80c8bf
craig-a-swanson <cswanson02@me.com>
2024-07-02 22:08:43 -0500

    Initial list of desserts from server

--- FetchProject/ContentView.swift
+++ FetchProject/ContentView.swift
@@ -8,17 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var desserts: [Meal]?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            if let desserts {
+                List(desserts, id: \.idMeal) { dessert in
+                    HStack {
+                        AsyncImage(url: URL(string: dessert.strMealThumb)) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 100, height: 100)
+
+                        Text(dessert.strMeal)
+                    }
+                }
+            } else {
+                Text("Loading desserts.")
+                    .font(.largeTitle)
+            }
         }
-        .padding()
+        .task {
+            do {
+                let meals = try await fetchDesserts()
+                desserts = meals
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    func fetchDesserts() async throws -> [Meal] {
+        let urlString = "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert"
+        guard let url = URL(string: urlString) else {
+            throw NetworkError.invalidURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let response = response as? HTTPURLResponse,
+           (200..<300).contains(response.statusCode) else {
+            throw NetworkError.invalidURLResponseCode
+        }
+        do {
+            let results = try JSONDecoder().decode(Results.self, from: data)
+            return results.meals
+        } catch {
+            throw NetworkError.decodingError
+        }
+        
     }
 }
 
-#Preview {
-    ContentView()
+enum NetworkError: Error {
+    case invalidURL
+    case invalidURLResponseCode
+    case decodingError
 }
